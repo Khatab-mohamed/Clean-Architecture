f4c45688f0c5991ac35a599e9920cdfcf138e818
Dev_Khatab <khatap1@hotmail.com>
2021-01-25 02:41:23 -0800

    Commit Service and Repository Interfaces, CouseViewModels

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
